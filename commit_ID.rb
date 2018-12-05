4b0cd17b4caf9d4dfbe6cb42e5e50c58f772ce6f
Sanjiv <sanjivkumarverma12@gmail.com>
2018-12-05 12:36:36 +0530

    added file for time

---
+++ commit_ID.rb
@@ -0,0 +1,2 @@
+commit=exec('git ls-remote https://github.com/sanjivkumar12/Time_Test.git HEAD');
+puts "#{commit}"
--- time.rb
+++ time.rb
@@ -1,5 +1,7 @@
-current_timestamp = Time.now
-current_timestamp1 = Time.now.strftime("%Y%m%d%H%M%S")
-puts "lets see what happens"
-puts "#{current_timestamp}"
-puts "#{current_timestamp1}"
+#current_timestamp = Time.now
+#current_timestamp1 = Time.now.strftime("%Y%m%d%H%M%S")
+current_timestamp2 = Time.now.strftime("Date:%Y/%m/%d Time:%H:%M:%S")
+#puts "lets see what happens"
+#puts "#{current_timestamp}"
+#puts "#{current_timestamp1}"
+puts "#{current_timestamp2}"
